823ecd68a266b09bae117df8898c00eaa297fefa
Jeremi Stadler <stadler.jeremi@gmail.com>
2023-01-03 01:09:22 +0100

    Update bench.ts

--- benchmark/bench.ts
+++ benchmark/bench.ts
@@ -6,20 +6,27 @@ const conn = createMysqlConnection({
   password: '123',
   database: 'test',
   port: 3306,
-  debug: true,
+  debug: false,
   ssl: false,
 });
 
 async function run() {
-  const r1 = await conn.query('SELECT * FROM test82t1');
-  const r2 = await conn.query('SELECT * FROM test82t1');
+  await conn.connect();
+
+  for (let ii = 0; ii < 5; ii++) {
+    const start = performance.now();
+    const ITERS = 10000;
+    for (let i = 0; i < ITERS; i++) {
+      await conn.query('SELECT * FROM test82t1');
+    }
+
+    const end = performance.now();
+    const timeDiff = end - start;
+
+    console.log((ITERS / (timeDiff / 1000)).toFixed(0), 'queries / s');
+  }
+
+  await conn.close();
 }
 
-conn
-  .connect()
-  .then(() => {})
-  .then((result) => {
-    console.log('GOT RESULT!', result);
-
-    return conn.query('SELECT * FROM test82t1');
-  });
+run();
