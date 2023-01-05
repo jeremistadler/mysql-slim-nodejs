94f37cc4fb4905818968036ff26846aca6926e6e
Jeremi Stadler <stadler.jeremi@gmail.com>
2023-01-05 01:37:33 +0100

    Update error.test.ts

--- test/error.test.ts
+++ test/error.test.ts
@@ -8,7 +8,7 @@ describe('Errors', () => {
   it('errors on unclosed quote', async () => {
     expect((await conn).query('SELECT "hello')).rejects.toEqual(
       new MysqlError(
-        "You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '\"hello' at line 13",
+        "You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '\"hello' at line 1",
         'ER_PARSE_ERROR',
         true
       )
